19e588cab0f0e949628c435218fe94a8a6aa1c75
Dannison Arias <56929623+dannisonarias@users.noreply.github.com>
2020-02-20 09:28:47 -0400

    added my_each_with_index method

--- main.rb
+++ main.rb
@@ -8,4 +8,12 @@ module Enumerable
       self
     end
     
+    def my_each_with_index
+      return to_enum :my_each_with_index unless block_given?
+      for i in 0...self.length
+        yield(i,self[i])
+      end
+      self
+    end
+    
 end
