53a2bdd15daf4d6f90f7ff7b8d6f6d56f16a0c3b
Dannison Arias <56929623+dannisonarias@users.noreply.github.com>
2020-02-20 09:29:34 -0400

    Added my_all method

--- main.rb
+++ main.rb
@@ -23,4 +23,11 @@ module Enumerable
       result
     end
     
+    def my_all
+      return to_enum :my_all unless block_given?
+      test_all=true
+      self.my_each{|i| test_all=false unless yield(i)}
+      p test_all
+    end
+    
 end
