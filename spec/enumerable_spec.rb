42466f5629d6fa8286d4d106015aeb4db532dd3a
Carlos Anriquez <canriquez@gmail.com>
2020-03-05 11:06:39 -0300

    added .rspec file. changed spec name to enable rspec from root. added my_each no block test

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,4 +1,4 @@
-require "../main"
+require "./main"
 RSpec.describe Enumerable do
     let(:array1) {[1,2,3,4,5,6,7,8,9,10]}
     let(:array_strings) {%w[cat dog monkey lion elephant]}
@@ -11,6 +11,14 @@ RSpec.describe Enumerable do
         it "test my each method with array of strings" do
             expect(array_strings.my_each{|i| p i+'1' }).to eql(array_strings.each{|i| p i+'2' })
         end
+
+        it "test my each method with no block given, expects #enumerator with same array" do
+            puts "same array - converts output to array"
+            expect(array_strings.my_each.to_a).to eql(array_strings.each.to_a)
+            puts "same class enumerator"
+            expect(array_strings.my_each.class).to eql(array_strings.each.class)
+        end
+
 
     end
 end 
